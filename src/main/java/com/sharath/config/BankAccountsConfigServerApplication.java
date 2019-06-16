172d07c68d22f9f2cc6cbe5e59047f7136667bd1
shar3939 <catchshar@gmail.com>
2019-06-16 12:43:10 +0530

    Bank App Config Server Initial Setup

---
+++ src/main/java/com/sharath/config/BankAccountsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.sharath.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountsConfigServerApplication.class, args);
+	}
+
+}
